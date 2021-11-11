f33e61a03198148b65f8fbf56ec9aa29822aabd5
DuskXi <DuskXi>
2021-11-11 14:50:11 +0000

    构建播放类

--- test.py
+++ test.py
@@ -6,6 +6,7 @@ from PyQt5.QtCore import QThread
 from PyQt5.QtGui import QImage, QPixmap
 from PyQt5.QtWidgets import QApplication
 
+from App import App
 from Video import Video
 from loguru import logger
 from moviepy.editor import VideoFileClip
@@ -45,9 +46,6 @@ class ThreadVideo(QThread):
             bytesPerLine = 3 * width
             qImg = QImage(frame.data, width, height, bytesPerLine, QImage.Format_BGR888)
             self.window.controls.video.setPixmap(QPixmap(qImg))
-            # sleepingTime = frameInterval - (time.time() - play_start)
-            # if sleepingTime > 0:
-            #     time.sleep(sleepingTime)
 
 
 def play_audio(audio):
@@ -62,8 +60,6 @@ def play_audio(audio):
     step = (1 / 44100)
     start = time.time()
     while time.time() - start < 100:
-        # while t > time.time() - start:
-        #     time.sleep(0.000001)
         t += step
         stream.write(audio.get_frame(t).astype('float32').tostring())
     p.terminate()
@@ -80,15 +76,18 @@ def onplay():
 
 
 def run_test():
-    global window
-    app = QApplication(sys.argv)
-    win = Window("ObjectDetectionAPIMarker")
-    window = win
-    win.listeners.onplay = onplay
-    win.loadUI(json.loads(read_file("layout.json")))
-
-    win.show()
-    app.exec_()
+    app = App("ObjectDetectionAPIMarker", "layout.json")
+    app.create_window(sys.argv)
+    app.Start()
+    # global window
+    # app = QApplication(sys.argv)
+    # win = Window("ObjectDetectionAPIMarker")
+    # window = win
+    # win.listeners.onplay = onplay
+    # win.loadUI(json.loads(read_file("layout.json")))
+    #
+    # win.show()
+    # app.exec_()
     # sys.exit()
 
     # video = Video(r"C:\Users\Dusk\Downloads\330925181_nb2-1-112.flv")
