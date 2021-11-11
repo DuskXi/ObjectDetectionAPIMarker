4e8eb63f9d9714b498eadeccb808e5172504502c
DuskXi <DuskXi>
2021-11-11 00:36:35 +0000

    构建视频类

---
+++ Video.py
@@ -0,0 +1,35 @@
+import os
+
+import cv2
+import numpy as np
+
+
+class Video:
+    def __init__(self, path):
+        self.path = path
+        self.width = -1
+        self.height = -1
+        self.fps = -1
+        self.duration = None
+        self.cap = None
+        self.numberFrames = None
+        self.currentFrame = 0
+
+    def load_video(self):
+        self.cap = cv2.VideoCapture(self.path)
+        self.get_video_info()
+
+    def get_video_info(self):
+        self.width = self.cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+        self.height = self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+        self.fps = self.cap.get(cv2.CAP_PROP_FPS)
+        self.numberFrames = self.cap.get(cv2.CAP_PROP_FRAME_COUNT)
+
+    def get_frame(self, index):
+        self.cap.set(cv2.CAP_PROP_POS_FRAMES, index)
+        ret, frame = self.cap.read()
+        return np.array(frame)
+
+    def get_next_frame(self):
+        self.currentFrame += 1
+        return self.get_frame(self.currentFrame)
