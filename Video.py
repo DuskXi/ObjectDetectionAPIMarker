5262be6816f954d160364a5ca13ff1a3426f8932
DuskXi <DuskXi>
2021-11-12 16:43:13 +0000

    增加初始化时抽取关键帧的函数

--- Video.py
+++ Video.py
@@ -5,6 +5,9 @@ import time
 import numpy as np
 from loguru import logger
 
+import numba
+from numba import jit
+
 
 class Video:
     def __init__(self, path):
@@ -17,16 +20,27 @@ class Video:
         self.numberFrames = None
         self.currentFrame = 0
         self.frameInterval = -1
+        self.timeLong = -1
+        self.thumbnail = []
 
     def load_video(self):
         self.cap = cv2.VideoCapture(self.path)
         self.get_video_info()
+        start = time.time()
+        self.generateThumbnail()
+        logger.info(f"耗时:{(time.time() - start) * 1000}ms")
 
     def get_video_info(self):
         self.width = self.cap.get(cv2.CAP_PROP_FRAME_WIDTH)
         self.height = self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
         self.fps = self.cap.get(cv2.CAP_PROP_FPS)
         self.numberFrames = self.cap.get(cv2.CAP_PROP_FRAME_COUNT)
+        self.timeLong = self.numberFrames / self.fps
+
+    def generateThumbnail(self):
+        for i in range(100):
+            self.thumbnail.append(self.get_frame(int(i * (self.numberFrames / 100))))
+        self.cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
 
     def get_frame(self, index):
         if self.cap.get(cv2.CAP_PROP_POS_FRAMES) != index:
@@ -36,7 +50,8 @@ class Video:
 
     def get_next_frame(self):
         self.currentFrame += 1
-        return self.get_frame(self.currentFrame)
+        ret, frame = self.cap.read()
+        return np.array(frame)
 
     def get_timestamps(self):
         return self.cap.get(cv2.CAP_PROP_POS_MSEC) / 1000
